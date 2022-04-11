7601ef76a6f123347450f53e1b815f137d7ff3fc
sooyeolPark <jump2766@gmail.com>
2022-04-11 20:19:15 +0900

    [MODIFY] index.js node.js ROUTER 추가하는 중 docker-compose build error 해결 중

--- node/index.js
+++ node/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require("./node_modules/express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("This is Node Server"));
 
 app.listen(port, () =>
     console.log(`Example app listening at http://localhost:${port}`)
