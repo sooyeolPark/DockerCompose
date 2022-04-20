4966ea46ff8f24bf8497df9441db3d8eff315d80
sooyeolPark <jump2766@gmail.com>
2022-04-20 23:30:13 +0900

    [MODIFY] docker-compose.yml, index.js, setup.sql mysql container에 node.js서버 연결 성공

--- mysql/init/setup.sql
+++ mysql/init/setup.sql
@@ -1,2 +1,13 @@
--- 동작 test
-CREATE DATABASE kikiki;
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'psy1234';
+FLUSH PRIVILEGES;
+
+CREATE DATABASE init_test;
+CREATE TABLE init_test.Users (
+    `idx` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'PK',
+    `id` varchar(20) NOT NULL COMMENT '아이디',
+    `password` varchar(30) NOT NULL COMMENT '비밀번호',
+    `name` varchar(20) NOT NULL COMMENT '이름',
+    `created_at` DATETIME NOT NULL DEFAULT NOW() COMMENT '생성일',
+    PRIMARY KEY (`idx`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO init_test.Users (`id`, `password`, `name`) VALUES ('clap10', 'psy1234', '수열이');
--- node/index.js
+++ node/index.js
@@ -4,23 +4,23 @@ const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
-// const connection = mysql.createConnection({
-//     host     : 'mysql',
-//     port     : '3306',
-//     user     : 'root',
-//     password : 'psy1234',
-//     database : 'first_test'
-// });
-// connection.connect();
-// let data;
-// connection.query('SELECT * from Users', (error, rows, fields) => {
-//     if (error) throw error;
-//     data = rows;
-// });
-//
-// connection.end();
-// app.get("/node", (req, res) => res.send(data));
-app.get("/node", (req, res) => res.send('Hi'));
+const connection = mysql.createConnection({
+    host     : 'mysql',
+    port     : '3306',
+    user     : 'root',
+    password : 'psy1234',
+    database : 'init_test'
+});
+connection.connect();
+let data;
+connection.query('SELECT * from Users', (error, rows, fields) => {
+    if (error) throw error;
+    data = rows;
+});
+
+connection.end();
+app.get("/node", (req, res) => res.send(data));
+
 
 app.listen(port, () =>
     console.log(`Example app listening at http://localhost:${port}`)
