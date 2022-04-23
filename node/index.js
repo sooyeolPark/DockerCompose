72c2d69ebcd8b47ef2f7e192ced218c676d7876c
sooyeolPark <jump2766@gmail.com>
2022-04-23 22:10:09 +0900

    [MODIFY] index.js node.js에서 DB연결 객체, 함수 등을 어떻게 만들면 좋을까? 고민중

--- node/index.js
+++ node/index.js
@@ -1,25 +1,33 @@
 const express = require("./node_modules/express");
 const mysql = require('mysql');
+let connection = mysql.createConnection({
+    host: 'mysql',
+    port: '3306',
+    user: 'root',
+    password: 'psy1234',
+    database: 'init_test'
+});
 
 const app = express();
 const port = 3000;
 
-const connection = mysql.createConnection({
-    host     : 'mysql',
-    port     : '3306',
-    user     : 'root',
-    password : 'psy1234',
-    database : 'init_test'
-});
-connection.connect();
-let data;
-connection.query('SELECT * from Users', (error, rows, fields) => {
-    if (error) throw error;
-    data = rows;
-});
+function sqlExec(sql, connection) {
 
-connection.end();
-app.get("/node", (req, res) => res.send(data));
+    connection.connect();
+    let data;
+    connection.query(sql, (error, result, fields) => {
+        if (error) throw error;
+        data = result;
+    });
+    connection.query()
+    connection.end();
+    return data
+}
+
+let insertSql = "INSERT INTO init_test.Users (`id`, `password`, `name`) VALUES ('test1', '1q2w3e', '테스트')";
+
+let result = sqlExec(insertSql);
+app.get("/node", (req, res) => res.send(result));
 
 
 app.listen(port, () =>
